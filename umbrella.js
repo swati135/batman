2afa02be851b687c50a8d6d09d25b3e40aa294bc
swati135 <34855416+swati135@users.noreply.github.com>
2021-01-20 16:56:50 +0530

    Update umbrella.js

--- umbrella.js
+++ umbrella.js
@@ -3,4 +3,9 @@ class Umbrella{
         this.body = Bodies.circle(x,y,100,{isStatic: true});
         World.add(world,this.body);
     }
+     display(){
+        var pos = this.umbrella.position;
+        imageMode(CENTER);
+        image(this.image,pos.x,pos.y+70,300,300);
+    }
 }
